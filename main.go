4ef49869b4803c0666ac8b91ca2ab17586908f80
Tapajyoti Ghosh <tapajyotighosh10@gmail.com>
2023-11-08 15:59:02 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/login",Login)
+	http.HandleFunc("/home",Home)
+	http.HandleFunc("/refresh",Refresh)
+
+	log.Fatal(http.ListenAndServe(":8000",nil))
+}
+
+
